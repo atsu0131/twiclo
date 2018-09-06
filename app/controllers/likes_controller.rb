87ba42aff58e589bed669db81dd291fd481ed9fb
EC2 Default User <ec2-user@ip-172-31-32-193.us-east-2.compute.internal>
2018-09-06 09:07:21 +0000

    for heroku

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success] = 'お気に入りしました。'
-    redirect_to user_path
+    redirect_to users_path
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to user_path
+    redirect_to users_path
   end
 end
